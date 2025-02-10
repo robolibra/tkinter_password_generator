43cdb8537ec03b9d55aa5dc4f8948dc6aa5fd365
Andrei Arkhipov <iamckuff@gmail.com>
2025-02-10 20:05:00 +0300

    Generate random password

--- generator.py
+++ generator.py
@@ -1,21 +1,43 @@
+import random
 import tkinter as tk
+import string
+from tkinter import IntVar, ttk 
 
-from tkinter import ttk 
+password_text = 'Ваш пароль будет здесь'
+pass_symbol = '!@#%^-_'
 
 
 def insert_text(text_widget: tk.Text, text: str):
     text_widget.config(state="normal")
     text_widget.delete("1.0", tk.END)
-    text_widget.insert("1.0", text)
+    text_widget.insert("1.0", "\n")
+    text_widget.insert("2.0", text, "center")
     text_widget.config(state="disabled")
 
 
-pass_symbol = '!@#%^-_'
+def button_click(length: str, upper: bool = True , numbers: bool = False, symbols: bool = False):
+    all_letters = string.ascii_uppercase * upper \
+    + string.digits * numbers \
+    + pass_symbol * symbols \
+    + string.ascii_lowercase
+    password = random.choice(string.ascii_lowercase) \
+    + random.choice(string.ascii_uppercase) * upper \
+    + random.choice(string.digits) * numbers \
+    + random.choice(pass_symbol) * symbols
+    password_text = password + ''.join(random.sample(all_letters, int(length) - len(password)))
+
+    password_list = list(password_text)  # Convert to a list of characters
+    random.shuffle(password_list)        # Shuffle the list in place
+    shuffled_password_text = ''.join(password_list)  # Convert back to a string
+    insert_text(generated_text, shuffled_password_text)
+
+
+
 
 
 root = tk.Tk()
 root.title('Генератор секрета')
-root.geometry('300x350')
+root.geometry('300x400')
 root.resizable(False, False)
 root.configure(bg='lightgray')
 
@@ -57,30 +79,41 @@ combo['values'] = list(range(min_length, max_length + 1))
 combo.set(min_length)
 
 
-generated_text = tk.Text(frame3, background= 'white', font='Helvetica, 13', height=2, state="disabled")
+generated_text = tk.Text(
+    frame3, background= 'white', font='Helvetica, 13', height=2, state="disabled"
+)
 generated_text.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
-insert_text(generated_text, 'Ваш пароль будет здесь')
+generated_text.tag_configure("center", justify="center")
+insert_text(generated_text, password_text)
 
-button1 = ttk.Button(frame3, text='Сгенерировать')
+button1 = ttk.Button(
+    frame3, text='Сгенерировать', command=lambda: button_click(
+        length=combo.get(), 
+        upper=var1.get(), 
+        numbers=var2.get(), 
+        symbols=var3.get()
+    )
+)
 button1.pack(pady=(0, 10))
+
+label2 = ttk.Label(frame4, text='Добавить символы')
+label2.pack(pady=10)
 
 checks = []
 
 var1 = tk.IntVar(value=1)
 check1 = ttk.Checkbutton(frame4, text='A..Z', variable=var1)
 checks.append(check1)
-var2 = tk.IntVar(value=0)
-check2 = ttk.Checkbutton(frame4, text='a..z', variable=var2)
+var2 = tk.IntVar(value=1)
+check2 = ttk.Checkbutton(frame4, text='0..9', variable=var2)
 checks.append(check2)
 var3 = tk.IntVar(value=1)
-check3 = ttk.Checkbutton(frame4, text='0..9', variable=var3)
+check3 = ttk.Checkbutton(frame4, text=pass_symbol, variable=var3)
 checks.append(check3)
-var4 = tk.IntVar(value=0)
-check4 = ttk.Checkbutton(frame4, text=pass_symbol, variable=var4)
-checks.append(check4)
+
 
 for check in checks:
-    check.pack(padx=10, pady=20, side=tk.LEFT)
+    check.pack(padx=15, pady=0, side=tk.LEFT)
     
 
 root.mainloop()
